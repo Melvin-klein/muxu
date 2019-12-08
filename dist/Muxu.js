5bf7c1b26a815bd9fecda44dc54c648854869ad0
Melvin <m.nargeot@archidvisor.com>
2019-12-09 00:52:28 +0100

    Update Muxu.js

--- dist/Muxu.js
+++ dist/Muxu.js
@@ -3,19 +3,16 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Router", {
-  enumerable: true,
-  get: function get() {
-    return _Router["default"];
-  }
-});
 exports["default"] = void 0;
 
 var _Router = _interopRequireDefault(require("./routing/Router"));
 
+var _MuxuRouterComponent = _interopRequireDefault(require("./routing/MuxuRouterComponent"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = {
-  Router: _Router["default"]
+  Router: _Router["default"],
+  MuxuRouterComponent: _MuxuRouterComponent["default"]
 };
 exports["default"] = _default;
